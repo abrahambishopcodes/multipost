44e5b436865cecf5c50835308f3767d6005a93fc
abrahambishopcodes <crypticcodetechnologies@gmail.com>
2026-05-28 22:53:56 +0100

    feat: implement global error handling middleware and custom AppError class

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,25 @@
-import express from "express"
+import express, {Request, Response, NextFunction} from "express"
+import AppError from "./utils/AppError"
 
 const app = express()
 const PORT = 3000
 
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
+})
+
+app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
+
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Something went wrong";
+
+    res.status(statusCode).json({
+        message,
+        status: err.status,
+        isOperational: err.isOperational,
+    })
 })
---
+++ src/utils/AppError.ts
@@ -0,0 +1,18 @@
+// AppError class
+
+class AppError extends Error {
+    public statusCode: number;
+    public status: string;
+    public isOperational: boolean;
+    public message: string;
+
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+        this.isOperational = true;
+        this.message = message;
+    }
+}
+
+export default AppError;
