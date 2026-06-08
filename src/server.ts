e53223b248596d1f5520377eaa797308b38084a1
abrahambishopcodes <crypticcodetechnologies@gmail.com>
2026-06-08 01:26:12 +0100

    feat: integrate MongoDB connection and update environment variable handling to support dynamic configuration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, {Request, Response, NextFunction} from "express"
 import AppError from "./utils/AppError"
 import AppRouter from "./router/index.router"
+import mongoose from "mongoose"
+import { getEnv } from "./utils/env"
 
 const app = express()
-const PORT = 3000
+const PORT = getEnv("PORT", "3000")
 
 // middlewares
 app.use(express.json())
@@ -12,8 +14,19 @@ app.use(express.urlencoded({extended: true}))
 // routes
 app.use("/api/v1", AppRouter)
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+const MONGODB_URI = getEnv("NODE_ENV", "development") === "development" ? getEnv("MONGO_DEV_URI") : getEnv("MONGO_PROD_URI");
+
+// connect to database then start server
+mongoose.connect(MONGODB_URI).then(
+    () => {
+        console.log("Database connected successfully")
+        // start server
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    }
+).catch((err) => {
+    console.log(err)
 })
 
 app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,9 +1,9 @@
 import "dotenv/config"
 
-export const getEnv = (key: string, defaultValue: string = "") => {
+export const getEnv = (key: string, defaultValue?: string ) => {
     const value = process.env[key] || defaultValue
 
-    if (value === "") {
+    if (value === undefined) {
         throw new Error(`Missing required environment variable: ${key}`)
     }
 
